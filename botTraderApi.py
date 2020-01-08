7f06b1d3bbfcec1cee01e9c5f804c3ae11664abd
MatheusVp2 <teteuoliveira12@gmail.com>
2020-01-08 01:16:45 -0300

    Atualização da API do BOT

---
+++ botTraderApi.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#####################################
+'''
+	Importações Gerais
+'''
+
+from time import strftime, time, sleep
+from datetime import datetime, timezone, timedelta
+import configparser
+
+#####################################
+
+
+#####################################
+'''
+	Informações sobre leitura das entradas
+'''
+#####################################
+
+# RETORNA AS ENTRADAS EM MATRIZ ( TIME | ATIVO | HORARIO | DIREÇAO )
+def getEntradas():
+	'''
+	RETORNAR MATRIZ DE ENTRADAS COM PADRAO ( TIME | ATIVO | HORARIO | DIREÇAO )
+	'''
+	arq = open('trader.txt', 'r')
+	entradas = arq.read().splitlines()
+	arq.close()
+
+	lista = []
+	for i in entradas:
+		lista.append(i.split(';'))
+
+	return lista
+
+
+#####################################
+'''
+	Funções para controle de horas
+'''
+#####################################
+
+# TRAVANDO O HORARIO COM O FUSO HORARIO UTC-3
+def getHoraNow():
+	'''
+	DEFINE COMO PADRAO O HORARIO UTC-3
+	'''
+	fuso = timezone(timedelta(hours=-3))
+	hora = datetime.now()
+	hora = hora.astimezone(fuso)
+	hora = hora.strftime( '%H:%M:%S' )
+	return hora
+
+
+# FUNÇÃO PARA VOLTAR A HORA E VERIFICAR O ATIVO 30 SEGUNDOS ANTES
+def timeVerificaAtivo(timeEntrada):
+	'''
+	ENTRA COM A HORA DA ENTRADA
+	E RETORNA 30 SEGUNDOS ANTES PARA VERIFICAÇÃO DO ATIVO
+	'''
+
+	# LOGICA PARA ERRO DE FORMATAÇÃO
+	if len( timeEntrada.split(":") ) == 3:
+		formato = "%H:%M:%S"
+	else:
+		formato = "%H:%M"
+	# LOGICA PARA VOLTA DO HORARIO VERIFICADOR
+	cHora     = datetime.strptime( timeEntrada , formato )
+	vHora     = cHora - timedelta(seconds=30)
+	timeAtivo = vHora.strftime( '%H:%M:%S' )
+	return timeAtivo
+
+
+# FUNÇÃO PARA PODER CRIAR O DELAY DA ENTRADA
+def delayEntrada(delay, timeEntrada):
+	'''
+	ENTRA COM O DELAY E A HORA DA ENTRADA
+	E RETORNA A NOVA HORA DA ENTRADA
+	'''
+
+	# LOGICA PARA ERRO DE FORMATAÇÃO
+	if len( timeEntrada.split(":") ) == 3:
+		formato = "%H:%M:%S"
+	else:
+		formato = "%H:%M"
+	# LOGICA PARA O DELAY
+	if delay <= 0:
+		cHora       = datetime.strptime( timeEntrada , formato )
+		timeEntrada = cHora.strftime( '%H:%M:%S' )
+		return timeEntrada
+	elif delay > 0 and delay <= 15:
+		cHora       = datetime.strptime( timeEntrada , formato )
+		vHora       = cHora - timedelta(seconds=delay)
+		timeEntrada = vHora.strftime( '%H:%M:%S' )
+		return timeEntrada
+	else:
+		cHora       = datetime.strptime( timeEntrada , formato )
+		vHora       = cHora - timedelta(seconds=15)
+		timeEntrada = vHora.strftime( '%H:%M:%S' )
+		return timeEntrada
+
+#####################################
+'''
+	Classe do Arquivo de Configuração
+'''
+#####################################
+
+class CFG():
+	
+	def __init__(self):
+		self.config = configparser.ConfigParser()
+		self.config.read("config.ini")
+
+	def getEmail(self):
+		return self.config["CONTA"]["EMAIL"]
+
+	def getSenha(self):
+		return self.config["CONTA"]["SENHA"]
+
+	def getTipoConta(self):
+		return self.config["CONTA"]["TIPO"]
+
+	def getDinheiro(self):
+		return self.config["ENTRADA"]["VALOR"]
